110b0477a15d61bd1ea0aa9763d59c593a74a236
leclaist <sleclaire06@gmail.com>
2015-11-22 16:45:41 -0600

    group; Passenger.cpp and Passenger.h filled out

--- Passenger.cpp
+++ Passenger.cpp
@@ -1,5 +1,31 @@
 //
 // Created by thisguy on 11/22/15.
 //
-
+#include <iostream>
 #include "Passenger.h"
+using namespace std;
+namespace Airport {
+    Passenger::Passenger() : firstName(""), lastName(""), ID(-1) {}
+    Passenger::Passenger(string fName, string lName, int idNo) : firstName(fName), lastName(lName), ID(idNo) {}
+
+    void Passenger::setFirstName(string newfirstName) {
+        firstName = newfirstName;
+    }
+    string Passenger::getFirstName() {
+        return firstName;
+    }
+    void Passenger::setLastName(string newLastName) {
+        lastName = newLastName;
+    }
+    string Passenger::getLastName() {
+        return lastName;
+    }
+    int Passenger::getId(){
+        return ID;
+    }
+    void Passenger::setId(int newId) {
+        ID = newId;
+    }
+
+
+}
--- Passenger.h
+++ Passenger.h
@@ -4,5 +4,29 @@
 
 #ifndef DATASTRUCTURESFINALPROJECT_PASSENGER_H
 #define DATASTRUCTURESFINALPROJECT_PASSENGER_H
+#include <iostream>
+#include "Flight.h"
+#include "Seat.h"
+#include <vector>
+namespace Airport {
+    class Passenger {
+    private:
+        std::string firstName;
+        std::string lastName;
+        int ID;
+        vector<Flight*> Flights;
+    public:
+        Passenger();
+        Passenger(std::string fName, std::string lName, int ID);
+        void updateFlight(Flight flightToChange);
+        std::string getFirstName();
+        void setFirstName(std::string newFirstName);
+        std::string getLastName();
+        void setLastName(std::string newLastName);
+        int getId();
+        void setId(int newId);
 
+    };
+
+}
 #endif //DATASTRUCTURESFINALPROJECT_PASSENGER_H
