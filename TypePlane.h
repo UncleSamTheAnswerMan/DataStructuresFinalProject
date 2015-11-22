64aee7589b73317ff2bc62f45c6abb5984226ea1
UncleSamTheAnswerMan <samuel.heying@live.bemidjistate.edu>
2015-11-22 17:29:36 -0600

    Group; TypePlane added, contains enum with values for total seat numbers

---
+++ TypePlane.h
@@ -0,0 +1,21 @@
+//
+// Created by thisguy on 11/22/15.
+//
+
+#ifndef DATASTRUCTURESFINALPROJECT_TYPEOFPLANES_H
+#define DATASTRUCTURESFINALPROJECT_TYPEOFPLANES_H
+
+namespace Airport {
+    static class TypePlane {
+    public:
+        static enum {
+            A380 = 900,
+            A330 = 253,
+            C130 = 92,
+            Boeing747 = 330,
+            Boeing737 = 162,
+            L10011 = 190
+        };
+    };
+}
+#endif //DATASTRUCTURESFINALPROJECT_TYPEOFPLANES_H
