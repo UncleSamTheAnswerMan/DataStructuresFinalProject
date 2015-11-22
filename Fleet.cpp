38fc90bf5997a78bcfb0404b16669c9b4e36fe0d
leclaist <sleclaire06@gmail.com>
2015-11-22 17:02:56 -0600

    group; Fleet.cpp and Fleet.h filled out

--- Fleet.cpp
+++ Fleet.cpp
@@ -1,5 +1,21 @@
 //
 // Created by thisguy on 11/22/15.
 //
-
+#include <iostream>
 #include "Fleet.h"
+using namespace std;
+namespace Airport {
+    Fleet::Fleet() : homePort(nullptr) {}
+    Fleet::Fleet(Aiport* port) : homePort(port) {}
+
+    void addPlane(Plane newPlane) {
+        Planes.push_back(newPlane);
+    }
+    void delPlane(Plane oldPlane) {
+        for (auto it=begin(Planes); it != end(Planes); ++it)
+            if ((*it)->getId() == oldPlane->getId()) {
+                Planes.erase(it);
+            }
+    }
+
+}
--- Fleet.h
+++ Fleet.h
@@ -4,5 +4,23 @@
 
 #ifndef DATASTRUCTURESFINALPROJECT_FLEET_H
 #define DATASTRUCTURESFINALPROJECT_FLEET_H
+#include <iostream>
+#include "Plane.h"
+#include "Passenger.h"
+#include "Flight.h"
+#include "Airport.h"
+#include <vector>
+namespace Airport {
+    class Fleet {
+    private:
+        vector<Plane*> Planes;
+        Airport* homePort;
+    public:
+        void addPlane(Plane newPlane);
+        void deletePlane(Plane oldPlane);
+    };
+
+
+}
 
 #endif //DATASTRUCTURESFINALPROJECT_FLEET_H
