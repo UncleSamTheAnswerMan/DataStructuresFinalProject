0f491e2874af8f2fce59e3a8a1411330e1613ece
UncleSamTheAnswerMan <samuel.heying@live.bemidjistate.edu>
2015-11-22 16:44:14 -0600

    Group; Seat base class filled out

--- Seat.h
+++ Seat.h
@@ -4,5 +4,31 @@
 
 #ifndef DATASTRUCTURESFINALPROJECT_SEAT_H
 #define DATASTRUCTURESFINALPROJECT_SEAT_H
+#include "Passenger.h"
+#include "Flight.h"
+namespace Airport {
+    class Seat {
+    public:
+        Seat();
+        Seat(const double basePrice);
+        Seat(const Passenger* occupant);
+        Seat(const Flight* flight);
+        Seat(const double basePrice, const Passenger* occupant, const Flight* flight);
+        Passenger* getOccupant() const;
+        double getBasePrice() const;
+        Flight* getFlight() const;
 
+        void setOccupant(const Passenger* occupant);
+        void setBasePrice(const double basePrice);
+        void setFlight(const Flight* flight);
+
+        double calcPrice(double days, double miles);
+
+    private:
+        Flight* theFlight;
+        double thePrice;
+        Occupant* theOccupant;
+
+    };
+}
 #endif //DATASTRUCTURESFINALPROJECT_SEAT_H
