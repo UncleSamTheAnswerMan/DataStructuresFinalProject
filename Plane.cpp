22ff7781d4a09bcbbcb3c4df813dea67390daea0
leclaist <sleclaire06@gmail.com>
2015-11-22 20:49:09 -0600

    group; Plane.cpp and Plane.h filled out

--- Plane.cpp
+++ Plane.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "Plane.h"
+#include <iostream>
+#include "Fleet.h"
+#include "Flight.h"
+#include "Seat.h"
+#include "TypePlane.h"
+#include "Itinerary.h"
+using namespace std;
+namespace Airport {
+    Plane::Plane() : model(nullptr), planeType(nullptr), associatedFleet(nullptr), ID(-1), itinerary(new Itinerary()), noOfSeats(-1), firstClass(-1), economyPlus(-1), economy(-1) {}
+    Plane::Plane(string thisModel, TypePlane thisType, Fleet* thisFleet, int thisId, int numbSeats, int fc, int ep, int e) : model(thisModel), planeType(thisType), associatedFleet(thisFleet), ID(thisId), itinerary(new Itinerary()), noOfSeats(numbSeats), firstClass(fc), economyPlus(ep), economy(e) {}
+
+    void Plane::setType(const TypePlane typePlane) {
+        planeType = typePlane;
+    }
+    TypePlane Plane::getType() const {
+        return planeType;
+    }
+    void Plane::setModel(const string newModel) {
+        model = newModel;
+    }
+    string Plane::getModel() const {
+        return model;
+    }
+    void Plane::setFleet(const Fleet* newFleet) {
+        associatedFleet = newFleet;
+    }
+    Fleet* Plane::getFleet() const {
+        return associatedFleet;
+    }
+    void Plane::setId(const int id) {
+        ID = id;
+    }
+    int Plane::getId() const {
+        return ID;
+    }
+    void Plane::showItinerary() {
+        int i = 0;
+        for (i = itinerary.getFlightList().begin(); i != itinerary.getFlightList().end; i++) {
+            (*i)->printFlight();
+        }
+    }
+    int Plane::getTotalNoSeats() const {
+        return noOfSeats;
+    }
+    int Plane::getFirstClass() const {
+        return firstClass;
+    }
+    int Plane::getEconomyPlus() const {
+        return economyPlus;
+    }
+    int Plane::getEconomy() const {
+        return economy;
+    }
+    void Plane::_updateSeatTypes() {
+        planeType.calcSeats(&firstClass, &economyPlus, &economy, noOfSeats);
+    }
+}
+
--- Plane.h
+++ Plane.h
@@ -4,5 +4,44 @@
 
 #ifndef DATASTRUCTURESFINALPROJECT_PLANE_H
 #define DATASTRUCTURESFINALPROJECT_PLANE_H
+#include <iostream>
+#include "Fleet.h"
+#include "Flight.h"
+#include "Seat.h"
+#include "TypePlane.h"
+#include "Itinerary.h"
 
+namespace Airport {
+    class Plane {
+    private:
+        std::string model;
+        TypePlane planeType;
+        Fleet* associatedFleet;
+        int ID;
+        Itinerary itinerary;
+        int noOfSeats;
+        int firstClass;
+        int economyPlus;
+        int economy;
+    public:
+        Plane();
+        Plane(std::string theModel, TypePlane theType, Fleet* theFleet, int theId, int numSeats, int fc, int ep, int e);
+        void setType(const TypePlane planeType);
+        TypePlane getType() const;
+        void setModel(const std::string newModel);
+        std::string getModel() const;
+        void setFleet(const Fleet* newFleet);
+        Fleet* getFleet() const;
+        void setId(int newId);
+        int getId() const;
+        void showItinerary();
+        int getTotalNoSeats() const;
+        int getFirstClass() const ;
+        int getEconomyPlus() const;
+        int getEconomy() const;
+        void _updateSeatTypes();
+
+
+    };
+}
 #endif //DATASTRUCTURESFINALPROJECT_PLANE_H
