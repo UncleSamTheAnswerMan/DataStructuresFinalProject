a5ffd393108b8b2d9126de8142d0f98746b263b0
UncleSamTheAnswerMan <samuel.heying@live.bemidjistate.edu>
2015-11-30 20:30:24 -0600

    Group;  Finished Itinerary class

--- Itinerary.cpp
+++ Itinerary.cpp
@@ -3,3 +3,71 @@
 //
 
 #include "Itinerary.h"
+namespace Airport {
+    Itinerary::Itinerary() { }
+
+    int Itinerary::addFlight(const Flight *flightToAdd) { //TODO add error checking to prevent paradoxes in the time-space continuum
+        vector<Flight*>::iterator iter;
+        for(iter = flightList.begin(); iter!=flightList.end(); iter++){
+            if ((*iter)->getID() == flightToAdd->getID()) {
+                cout << "Flight already added. What were you thinking?" << endl;
+                return -1;
+            }
+        }
+        flightList.push_back(flightToAdd);
+        return 0;
+    }
+
+    int Itinerary::deleteFlight(const Flight *flightToDelete) {
+        vector<Flight*>::iterator iter;
+        for(iter = flightList.begin(); iter!=flightList.end(); iter++){
+            if ((*iter)->getID() == flightToDelete->getID()) {
+                flightList.erase(iter);
+                return 0;
+            }
+        }
+        cout << "Flight was not found, unable to delete. Try again with better input.";
+        return -1;
+    }
+
+    Flight* Itinerary::findFlightByTime(const time_t timeToFind) {
+        vector<Flight*>::iterator iter;
+        for (iter = flightList.begin(); iter!=flightList.end(); iter++){
+            if ((*iter)->getDepart() == timeToFind) {
+                return (*iter);
+            }
+        }
+        cout<<"Flight not found..." << endl;
+        return nullptr;
+    }
+
+    Flight* Itinerary::findFlightByID(const int flightID) {
+        vector<Flight*>::iterator iter;
+        for(iter = flightList.begin(); iter!=flightList.end(); iter++) {
+            if ((*iter)->getID() == flightID) {
+                return (*iter);
+            }
+        }
+        cout << "Flight not found..." << endl;
+        return nullptr;
+    }
+
+    void Itinerary::showAllFlights() const {
+        vector<Flight*>::iterator iter;
+        for(iter = flightList.begin(); iter!=flightList.end(); iter++){
+            (*iter)->printFlight();
+        }
+    }
+
+    void Itinerary::showNextFlight() const{
+        Flight* nextFlight = flightList[0];
+        vector<Flight*>::iterator iter;
+        for(iter = flightList.begin(); iter!=flightList.end(); iter++) {
+            if ((*iter)->getDepart() < nextFlight->getDepart()) {
+                nextFlight = (*iter);
+            }
+        }
+
+        nextFlight->printFlight();
+    }
+}
--- Itinerary.h
+++ Itinerary.h
@@ -5,4 +5,23 @@
 #ifndef DATASTRUCTURESFINALPROJECT_ITINERARY_H
 #define DATASTRUCTURESFINALPROJECT_ITINERARY_H
 
+
+#include "Flight.h"
+
+namespace Airport {
+
+    class Itinerary {
+    public:
+        Itinerary();
+        int addFlight(const Flight* flightToAdd);
+        int deleteFlight(const Flight* flightToDelete);
+        Flight* findFlightByTime(const time_t timeToFind);
+        Flight* findFlightByID(const int flightID);
+        void showAllFlights()const;
+        void showNextFlight()const;
+        //TODO add sort method
+    private:
+        vector<Flight*> flightList;
+    };
+}
 #endif //DATASTRUCTURESFINALPROJECT_ITINERARY_H
